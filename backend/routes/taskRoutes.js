d898dea53576c7c66bec5079c6e087535ad9a699
shreyas <shekar.sh@northeastern.edu>
2025-03-31 15:01:27 -0400

    login api

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -6,5 +6,6 @@ router.get("/", taskController.getAllTasks);
 router.post("/", taskController.createTask);
 router.put("/:taskID", taskController.updateTaskStatus);
 router.delete("/:taskID", taskController.deleteTask);
+router.post("/addUser", taskController.addUser);
 
 module.exports = router;
